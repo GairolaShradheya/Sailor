ad4715df55ca513fd0f675cda6e055f93eda17fe
Shradheya Gairola <gairolashradheya2006@gmail.com>
2024-10-20 22:09:54 +0530

    First update in website navbar and foote added

---
+++ app/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <footer className='flex px-10 bg-gray-900 h-[10vh] text-white items-center justify-center'>
+      <p>&copy;Donate Life || All rights are reserved</p>
+    </footer>
+  )
+}
+
+export default Footer
