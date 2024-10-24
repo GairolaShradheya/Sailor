5c578d7a9a9594483dd2dc95f7431eda1989e127
Shradheya Gairola <gairolashradheya2006@gmail.com>
2024-10-24 17:20:41 +0530

    welcome page updaated

--- app/page.js
+++ app/page.js
@@ -1,14 +1,18 @@
+"use client";
 import Image from "next/image";
+import { useRef } from "react";
 
 export default function Home() {
+  const ref = useRef("");
   return (
     <>
-    <div className="relative h-[100vh] w-full bg-slate-950 text-white">
-        <div className="absolute bottom-0 left-[-20%] right-0 top-[-10%] h-[500px] w-[500px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(255,0,182,.15),rgba(255,255,255,0))]">
+      <div className="w-full h-full flex flex-col p-20 items-center gap-10">
+        <div className="text-center">
+          <h2 className="text-[80px] pb-4 font-bold">Welcome to Sale<span className="text-violet-500">Sailor</span></h2>
+          <p className="text-[20px]">This is a beautiful e-commerce website for all the lovely people.</p>
+          <p className="text-[20px]">Have a look at our products.</p>
         </div>
-        Work on progress
-        <div className="absolute bottom-0 right-[-20%] top-[-10%] h-[500px] w-[500px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(255,0,182,.15),rgba(255,255,255,0))]">
-        </div>
+        <button className='border border-white px-4 py-2 rounded-full flex items-center bg-purple-700'>Get Started</button>
       </div>
     </>
   );
